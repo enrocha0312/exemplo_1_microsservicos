23fdfc3cb3357c50df02716bd09e8f2694ec1796
enrocha0312 <enrocha0312@gmail.com>
2024-07-22 06:34:56 -0300

    comecando o microsservicos de worker

---
+++ hr-worker/src/test/java/com/exemplo1/hr_worker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo1.hr_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
